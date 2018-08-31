c0056638863919fd40def84ce5f0d1170ba37779
Aperjump <wei@aperjump.me>
2018-08-31 12:29:17 -0700

    initial commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aperjump/Downloads/cmudatabase/project
-# Build directory: /home/aperjump/Downloads/cmudatabase/project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aperjump/Downloads/cmudatabase/project/src
-# Build directory: /home/aperjump/Downloads/cmudatabase/project/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
